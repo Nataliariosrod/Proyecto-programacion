cc5c952b9e010fc39e7719cb0e3e868f8c7141ee
Nataliariosrod <natriorod@gmail.com>
2020-02-03 23:14:58 +0100

    Version 1 Natalia

---
+++ src/dam/obj/Grupo.java
@@ -0,0 +1,13 @@
+package dam.obj;
+
+import java.time.LocalDate;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Grupo {
+
+	String nombre;
+	String pais;
+	Map<LocalDate, Cd> discografia = new TreeMap<>();
+	
+}
