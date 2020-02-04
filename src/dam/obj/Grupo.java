c4b048072df73afb313c0eef46f1cabafe26bbbd
Francisco José Caro Carazo <56670367+FranciscoJoseCaroCarazo@users.noreply.github.com>
2020-02-04 16:38:54 +0100

    introducir cd

--- src/dam/obj/Grupo.java
+++ src/dam/obj/Grupo.java
@@ -4,11 +4,14 @@ import java.time.LocalDate;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class Grupo {
+import ejercicio_3.ControlExcepciones;
+
+public class Grupo extends {
 
 	private String nombre;
 	private String pais;
-	private Map<LocalDate, Cd> discografia = new TreeMap<>();
+	private Map<LocalDate, String> discografia = new TreeMap<>();
+	private ControlExcepciones ex = new ControlExcepciones();
 	
 	Grupo(){
 		
@@ -36,6 +39,46 @@ public class Grupo {
 		this.pais = pais;
 	}
 	
+	public String DatosGrup() {
+		return ""+nombre+" ("+pais+")";
+		
+	}
+	
+	public void introducirCD(Cd cd) {
+		LocalDate fecha = cd.getFecha();
+		String nombre = cd.getNombre();
+		LocalDate temkey = null;
+		for(Map.Entry<LocalDate, String> i : discografia.entrySet()) {
+			if (i.getKey().equals(fecha)) {
+				temkey = i.getKey();
+			}
+		}if (temkey != null) {
+			
+		int key;
+		System.out.println("Ya existe un CD con esa misma fecha de lanzamiento /n¿Desea Sobreescribirlo?");
+			while (key == 0) {
+				System.out.println("¿Que deseea hacer?/n1.Sobreescribirlo/n2.Cancelar");
+				key = ex.controlInt();
+				switch (key) {
+				case 1:
+					discografia.put(fecha ,nombre);
+					System.out.println("El CD se ha sobreescrito con éxito");
+					break;
+					
+				case 2:
+					System.out.println("Cancelando operación");
+					break;
+	
+				default:
+					System.out.println("Error. Intriduzca una opción correcta");
+					break;
+				}	
+			}
+		}else{
+			discografia.put(fecha, nombre);
+		}
+	}		
+}
 	
 
-}
+
